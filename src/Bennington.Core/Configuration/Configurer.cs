77e93bafee3cf500caf246c27c31dad8076b019d
Tyrone Groves <grovest@digitalev.com>
2011-10-30 15:19:55 -0500

    added common configuration classes

---
+++ src/Bennington.Core/Configuration/Configurer.cs
@@ -0,0 +1,32 @@
+﻿using MvcTurbine.ComponentModel;
+using System.Collections.Generic;
+
+namespace Bennington.Core.Configuration
+{
+    public class Configurer
+    {
+        private static readonly Configurer DefaultConfigurer = new Configurer(null);
+        private readonly List<Configurer> configurers = new List<Configurer>();
+
+        public Configurer(Configurer parentConfigurer)
+        {
+            if(parentConfigurer != null)
+                parentConfigurer.configurers.Add(this);
+        }
+
+        public static Configurer Configure
+        {
+            get { return DefaultConfigurer; }
+        }
+
+        public IServiceLocator ServiceLocator
+        {
+            get { return ServiceLocatorManager.Current; }
+        }
+
+        public virtual void Run()
+        {
+            configurers.ForEach(configurer => configurer.Run());
+        }
+    }
+}
