7c069865f9fbc13f49ec398814cce7258d306e6c
Tyrone Groves <grovest@digitalev.com>
2011-10-30 15:34:43 -0500

    added caching components

---
+++ src/Bennington.Core/Caching/InvalidateCacheClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Bennington.Core.Caching
+{
+    public class InvalidateCacheClient
+    {
+        public void Invalidate(Uri invalidateCacheUri)
+        {
+            Invalidate(invalidateCacheUri, string.Empty);
+        }
+
+        public void Invalidate(Uri invalidateCacheUri, string cacheKey)
+        {
+            var client = new ChannelFactory<IInvalidateCacheService>(new NetNamedPipeBinding(), new EndpointAddress(invalidateCacheUri)).CreateChannel();
+            client.Invalidate(cacheKey);
+        }
+    }
+}
---
+++ src/Bennington.Core/Caching/InvalidateCacheService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Bennington.Core.Caching
+{
+    [ServiceContract]
+    public interface IInvalidateCacheService
+    {
+        [OperationContract]
+        void Invalidate(string cacheKey);
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class InvalidateCacheService : IInvalidateCacheService
+    {
+        private readonly Action<string> invalidateCache;
+
+        public InvalidateCacheService(Action<string> invalidateCache)
+        {
+            this.invalidateCache = invalidateCache;
+        }
+
+        public void Invalidate(string cacheKey)
+        {
+            invalidateCache(cacheKey);
+        }
+    }
+}
