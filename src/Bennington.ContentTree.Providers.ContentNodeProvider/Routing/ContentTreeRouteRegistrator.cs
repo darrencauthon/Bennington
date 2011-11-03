8c67650a73f736417b32eea478d29a446fc49173
Ferris Lucas <lucasf@digitalev.com>
2011-11-02 22:17:11 -0500

    get content to display when when routes match url

---
+++ src/Bennington.ContentTree.Providers.ContentNodeProvider/Routing/ContentTreeRouteRegistrator.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using MvcTurbine.Routing;
+
+namespace Bennington.ContentTree.Providers.ContentNodeProvider.Routing
+{
+    public class ContentTreeRouteRegistrator : IRouteRegistrator
+    {
+		public const int MaxDepthForContentTreeUrlSegments = 25;
+
+    	public void Register(RouteCollection routes)
+        {
+            routes.MapRoute(
+                "ContentTreeNode", 
+                "ContentTreeNode/{action}", 
+                new { controller = "ContentTreeNode", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
