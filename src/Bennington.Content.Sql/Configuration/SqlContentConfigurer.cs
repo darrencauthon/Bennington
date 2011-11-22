691ca6b2c33865b93c0d20280b12baf38d9a2386
Ferris Lucas <lucasf@digitalev.com>
2011-11-22 14:32:07 -0600

    create tables necessary for content routing if they do not already exist

--- src/Bennington.Content.Sql/Configuration/SqlContentConfigurer.cs
+++ src/Bennington.Content.Sql/Configuration/SqlContentConfigurer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Bennington.Content.Configuration;
 using Bennington.Content.Data;
+using Bennington.Content.Sql.Data;
 using Bennington.Core.Configuration;
 
 namespace Bennington.Content.Sql.Configuration
@@ -31,11 +33,104 @@ namespace Bennington.Content.Sql.Configuration
 
         public override void Run()
         {
+            EnsureNecessaryTablesExist(connectionString);
 
             ServiceLocator.Register<IContentTreeProvider>(new SqlContentTreeProvider(connectionString, invalidateCacheUrl));
             ServiceLocator.Register<IContentTypeRegistry>(new SqlContentTypeRegistry(connectionString));
 
             base.Run();
+        }
+
+        private void EnsureNecessaryTablesExist(string connectionString)
+        {
+            using (var dataContext = new ContentDataContext(connectionString))
+            {
+                CreateContentTreeTableIfNecessary(dataContext);
+                CreateContentTypesTableIfNecessary(dataContext);
+                CreateContentActionsTableIfNecessary(dataContext);
+            }
+        }
+
+        private void CreateContentTypesTableIfNecessary(ContentDataContext dataContext)
+        {
+            try
+            {
+                var items = dataContext.ContentTypeItems.Take(1).ToArray();
+            }
+            catch (Exception exception)
+            {
+                if (exception.Message == "Invalid object name 'dbo.ContentTypes'.")
+                {
+                    dataContext.ExecuteCommand(string.Format(@"
+                                        CREATE TABLE [dbo].[ContentTypes](
+	                                        [ContentTypeId] [int] IDENTITY(1,1) NOT NULL,
+	                                        [Type] [nvarchar](500) NULL,
+	                                        [ControllerName] [nvarchar](500) NULL,
+	                                        [DisplayName] [nvarchar](500) NULL,
+                                         CONSTRAINT [PK_ContentTypeItem] PRIMARY KEY CLUSTERED 
+                                        (
+	                                        [ContentTypeId] ASC
+                                        )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+                                        ) ON [PRIMARY]
+                                        "), new object[] { });
+                }
+            }
+        }
+
+        private void CreateContentActionsTableIfNecessary(ContentDataContext dataContext)
+        {
+            try
+            {
+                var items = dataContext.ContentActionItems.Take(1).ToArray();
+            }
+            catch (Exception exception)
+            {
+                if (exception.Message == "Invalid object name 'dbo.ContentActions'.")
+                {
+                    dataContext.ExecuteCommand(string.Format(@"
+                                        CREATE TABLE [dbo].[ContentActions](
+	                                        [ContentActionId] [int] IDENTITY(1,1) NOT NULL,
+	                                        [ContentTypeId] [int] NULL,
+	                                        [ContentType] [nvarchar](500) NULL,
+	                                        [Action] [nvarchar](500) NULL,
+	                                        [DisplayName] [nvarchar](500) NULL,
+                                            CONSTRAINT [PK_ContentActionItem] PRIMARY KEY CLUSTERED 
+                                        (
+	                                        [ContentActionId] ASC
+                                        )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+                                        ) ON [PRIMARY]
+                                        "), new object[] { });
+                }
+            }
+        }
+
+
+        private void CreateContentTreeTableIfNecessary(ContentDataContext dataContext)
+        {
+            try
+            {
+                var items = dataContext.ContentTreeItems.Take(1).ToArray();
+            }catch(Exception exception)
+            {
+                if (exception.Message == "Invalid object name 'dbo.ContentTree'.")
+                {
+                    dataContext.ExecuteCommand(string.Format(@"
+                                        CREATE TABLE [dbo].[ContentTree](
+	                                        [Id] [nvarchar](50) NOT NULL,
+	                                        [ParentId] [nvarchar](50) NOT NULL,
+	                                        [Segment] [nvarchar](500) NULL,
+	                                        [Action] [nvarchar](500) NULL,
+	                                        [Controller] [nvarchar](500) NULL,
+	                                        [TreeNodeId] [nvarchar](50) NULL,
+	                                        [ActionId] [nvarchar](50) NULL,
+                                            CONSTRAINT [PK_ContentTreeItem] PRIMARY KEY CLUSTERED 
+                                        (
+	                                        [Id] ASC
+                                        )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+                                        ) ON [PRIMARY]
+                                        "), new object[]{});
+                }
+            }
         }
     }
 }
