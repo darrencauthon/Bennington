e1d27a63495426426577bd84b08b78c4afea5d45
Ferris Lucas <ferris.lucas@gmail.com>
2011-12-06 20:41:19 -0600

    register user management top right menu icon

---
+++ src/Bennington.Cms.PrincipalProvider/IconMenuRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Bennington.Cms.MenuSystem;
+
+namespace Bennington.Cms.PrincipalProvider
+{
+    public class MenuSystemConfigurer : IMenuSystemConfigurer
+    {
+        public void Configure(IMenuRegistry sectionMenuRegistry)
+        {
+            sectionMenuRegistry.Add(new ActionIconMenuItem("User Management", "~/Content/Canvas/UserManagementIcon.gif", "Index", "User"));
+        }
+    }
+}
