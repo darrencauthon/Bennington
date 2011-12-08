7725706708e8197c2bb2518324529acb8de3c9c4
Ferris Lucas <ferris.lucas@gmail.com>
2011-12-08 11:04:16 -0600

    begin implementing ContentTreeNodeDisplayMetaViewModelBuilder

---
+++ src/Bennington.ContentTree.Providers.ContentNodeProvider.Tests/ViewModelBuilders/ContentTreeNodeDisplayMetaViewModelBuilderTests_BuildViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMoq;
+using Bennington.ContentTree.Providers.ContentNodeProvider.ViewModelBuilders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bennington.ContentTree.Providers.ContentNodeProvider.Tests.ViewModelBuilders
+{
+    [TestClass]
+    public class ContentTreeNodeDisplayMetaViewModelBuilderTests_BuildViewModel
+    {
+        private AutoMoqer mocker;
+
+        [TestInitialize]
+        public void Init()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = mocker.Resolve<ContentTreeNodeDisplayMetaViewModelBuilder>().BuildViewModel("treeNodeId", "actionId");
+
+            throw new NotImplementedException();
+        }
+    }
+}
