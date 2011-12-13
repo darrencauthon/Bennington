169b2708603f4a60944b2b75a1e1b09bd3e54ebf
Ferris Lucas <ferris.lucas@gmail.com>
2011-12-12 18:50:42 -0600

    implement [HtmlEditor] attribute

---
+++ src/SampleCmsWebsite/Controllers/SomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleCmsWebsite.Models;
+
+namespace SampleCmsWebsite.Controllers
+{
+    public class SomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index", new SomeControllerIndexViewModel());
+        }
+    }
+}
---
+++ src/SampleCmsWebsite/Models/SomeControllerIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bennington.Cms.Metadata;
+
+namespace SampleCmsWebsite.Models
+{
+    public class SomeControllerIndexViewModel
+    {
+        public SomeForm SomeForm { get; set; }
+    }
+
+    public class SomeForm
+    {
+        [HtmlEditor]
+        public string SomeProperty { get; set; }
+    }
+}
