0ad17b8f7f48914c590d83cbfe3e2d3b1e5faa1c
Ferris Lucas <ferris.lucas@gmail.com>
2012-01-06 16:50:19 -0600

    implement caching in ContentNodeProviderPublishedVersionRepository

--- src/Bennington.ContentTree.Providers.ContentNodeProvider/Repositories/ContentNodeProviderPublishedVersionRepository.cs
+++ src/Bennington.ContentTree.Providers.ContentNodeProvider/Repositories/ContentNodeProviderPublishedVersionRepository.cs
@@ -1,43 +1,62 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.Caching;
 using Bennington.ContentTree.Providers.ContentNodeProvider.Data;
 
 namespace Bennington.ContentTree.Providers.ContentNodeProvider.Repositories
 {
-	public interface IContentNodeProviderPublishedVersionRepository
-	{
-		IQueryable<ContentNodeProviderPublishedVersion> GetAllContentNodeProviderPublishedVersions();
-		void Update(ContentNodeProviderPublishedVersion instance);
-		void Create(ContentNodeProviderPublishedVersion instance);
-		void Delete(ContentNodeProviderPublishedVersion instance);
-	}
+    public interface IContentNodeProviderPublishedVersionRepository
+    {
+        IQueryable<ContentNodeProviderPublishedVersion> GetAllContentNodeProviderPublishedVersions();
+        void Update(ContentNodeProviderPublishedVersion instance);
+        void Create(ContentNodeProviderPublishedVersion instance);
+        void Delete(ContentNodeProviderPublishedVersion instance);
+    }
 
-	public class ContentNodeProviderPublishedVersionRepository : IContentNodeProviderPublishedVersionRepository
-	{
-		private readonly IDataModelDataContext dataModelDataContext;
+    public class ContentNodeProviderPublishedVersionRepository : IContentNodeProviderPublishedVersionRepository
+    {
+        private readonly IDataModelDataContext dataModelDataContext;
+        private readonly ObjectCache cache = MemoryCache.Default;
 
-		public ContentNodeProviderPublishedVersionRepository(IDataModelDataContext dataModelDataContext)
-		{
-			this.dataModelDataContext = dataModelDataContext;
-		}
+        public ContentNodeProviderPublishedVersionRepository(IDataModelDataContext dataModelDataContext)
+        {
+            this.dataModelDataContext = dataModelDataContext;
+        }
 
-		public IQueryable<ContentNodeProviderPublishedVersion> GetAllContentNodeProviderPublishedVersions()
-		{
-			return dataModelDataContext.ContentNodeProviderPublishedVersions;
-		}
+        public IQueryable<ContentNodeProviderPublishedVersion> GetAllContentNodeProviderPublishedVersions()
+        {
+            var items = cache["ContentNodeProviderPublishedVersions"] as IQueryable<ContentNodeProviderPublishedVersion>;
 
-		public void Update(ContentNodeProviderPublishedVersion instance)
-		{
-			dataModelDataContext.Update(instance);
-		}
+            if (items == null)
+            {
+                items = dataModelDataContext.ContentNodeProviderPublishedVersions;
 
-		public void Create(ContentNodeProviderPublishedVersion instance)
-		{
-			dataModelDataContext.Create(instance);
-		}
+                var localWorkingFolder = Path.Combine(ConfigurationManager.AppSettings["Bennington.LocalWorkingFolder"], @"BenningtonData\");
+                var policy = new CacheItemPolicy();
 
-		public void Delete(ContentNodeProviderPublishedVersion instance)
-		{
-			dataModelDataContext.Delete(instance);
-		}
-	}
+                policy.ChangeMonitors.Add(new HostFileChangeMonitor(new List<string> { localWorkingFolder }));
+
+                cache.Add("ContentNodeProviderPublishedVersions", items, policy);
+            }
+
+            return items;
+        }
+
+        public void Update(ContentNodeProviderPublishedVersion instance)
+        {
+            dataModelDataContext.Update(instance);
+        }
+
+        public void Create(ContentNodeProviderPublishedVersion instance)
+        {
+            dataModelDataContext.Create(instance);
+        }
+
+        public void Delete(ContentNodeProviderPublishedVersion instance)
+        {
+            dataModelDataContext.Delete(instance);
+        }
+    }
 }
