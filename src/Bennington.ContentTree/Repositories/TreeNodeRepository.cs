49f76107902614e8eec03ad9fd457a295d1304ce
Ferris Lucas <ferris.lucas@gmail.com>
2012-01-06 16:56:04 -0600

    implement caching in TreeNodeRepository

--- src/Bennington.ContentTree/Repositories/TreeNodeRepository.cs
+++ src/Bennington.ContentTree/Repositories/TreeNodeRepository.cs
@@ -1,44 +1,63 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.Caching;
 using Bennington.ContentTree.Data;
 
 namespace Bennington.ContentTree.Repositories
 {
-	public interface ITreeNodeRepository
-	{
-		IQueryable<TreeNode> GetAll();
-		TreeNode Create(TreeNode treeNode);
-		void Delete(string id);
-		void Update(TreeNode treeNode);
-	}
+    public interface ITreeNodeRepository
+    {
+        IQueryable<TreeNode> GetAll();
+        TreeNode Create(TreeNode treeNode);
+        void Delete(string id);
+        void Update(TreeNode treeNode);
+    }
 
-	public class TreeNodeRepository : ITreeNodeRepository
-	{
-		private readonly IDataModelDataContext dataModelDataContext;
+    public class TreeNodeRepository : ITreeNodeRepository
+    {
+        private readonly IDataModelDataContext dataModelDataContext;
+        private readonly ObjectCache cache = MemoryCache.Default;
 
-		public TreeNodeRepository(IDataModelDataContext dataModelDataContext)
-		{
-			this.dataModelDataContext = dataModelDataContext;
-		}
+        public TreeNodeRepository(IDataModelDataContext dataModelDataContext)
+        {
+            this.dataModelDataContext = dataModelDataContext;
+        }
 
-		public IQueryable<TreeNode> GetAll()
-		{
-			return dataModelDataContext.TreeNodes;
-		}
+        public IQueryable<TreeNode> GetAll()
+        {
+            var treeNodes = cache["TreeNodes"] as IQueryable<TreeNode>;
 
-		public TreeNode Create(TreeNode treeNode)
-		{
-			dataModelDataContext.Create(treeNode);
-			return treeNode;
-		}
+            if (treeNodes == null)
+            {
+                treeNodes = dataModelDataContext.TreeNodes;
 
-		public void Delete(string id)
-		{
-			dataModelDataContext.Delete(id);
-		}
+                var localWorkingFolder = Path.Combine(ConfigurationManager.AppSettings["Bennington.LocalWorkingFolder"], @"BenningtonData\");
+                var policy = new CacheItemPolicy();
 
-		public void Update(TreeNode treeNode)
-		{
-			dataModelDataContext.Update(treeNode);
-		}
-	}
+                policy.ChangeMonitors.Add(new HostFileChangeMonitor(new List<string> { localWorkingFolder }));
+
+                cache.Add("TreeNodes", treeNodes, policy);
+            }
+
+            return treeNodes;
+        }
+
+        public TreeNode Create(TreeNode treeNode)
+        {
+            dataModelDataContext.Create(treeNode);
+            return treeNode;
+        }
+
+        public void Delete(string id)
+        {
+            dataModelDataContext.Delete(id);
+        }
+
+        public void Update(TreeNode treeNode)
+        {
+            dataModelDataContext.Update(treeNode);
+        }
+    }
 }
