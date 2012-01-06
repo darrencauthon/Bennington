6c811022b7ca8b623f8998443010516eabdf644b
Ferris Lucas <ferris.lucas@gmail.com>
2012-01-06 16:53:58 -0600

    implement caching in ToolLinkProviderDraftRepository

--- src/Bennington.ContentTree.Providers.ToolLinkNodeProvider/Repositories/ToolLinkProviderDraftRepository.cs
+++ src/Bennington.ContentTree.Providers.ToolLinkNodeProvider/Repositories/ToolLinkProviderDraftRepository.cs
@@ -1,29 +1,48 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Runtime.Caching;
 using Bennington.ContentTree.Providers.ToolLinkNodeProvider.Data;
-using Bennington.ContentTree.Providers.ToolLinkNodeProvider.Models;
-using Bennington.Core.EntityFramework;
 using Bennington.Core.Helpers;
 using Bennington.Repository;
 using Bennington.Repository.Helpers;
 
 namespace Bennington.ContentTree.Providers.ToolLinkNodeProvider.Repositories
 {
-	public interface IToolLinkProviderDraftRepository
-	{
-		IQueryable<Data.ToolLinkProviderDraft> GetAll();
-	    string SaveAndReturnId(ToolLinkProviderDraft toolLinkProviderDraft);
-	}
+    public interface IToolLinkProviderDraftRepository
+    {
+        IQueryable<ToolLinkProviderDraft> GetAll();
+        string SaveAndReturnId(ToolLinkProviderDraft toolLinkProviderDraft);
+    }
 
     public class ToolLinkProviderDraftRepository : ObjectStore<ToolLinkProviderDraft>, IToolLinkProviderDraftRepository
-	{
-	    public ToolLinkProviderDraftRepository(IXmlFileSerializationHelper xmlFileSerializationHelper, IGetDataPathForType getDataPathForType, IGetValueOfIdPropertyForInstance getValueOfIdPropertyForInstance, IGuidGetter guidGetter, IFileSystem fileSystem) : base(xmlFileSerializationHelper, getDataPathForType, getValueOfIdPropertyForInstance, guidGetter, fileSystem)
-	    {
-	    }
+    {
+        private readonly ObjectCache cache = MemoryCache.Default;
 
-	    public IQueryable<ToolLinkProviderDraft> GetAll()
-	    {
-	        return base.GetAll().AsQueryable();
-	    }
-	}
+        public ToolLinkProviderDraftRepository(IXmlFileSerializationHelper xmlFileSerializationHelper, IGetDataPathForType getDataPathForType,
+                                               IGetValueOfIdPropertyForInstance getValueOfIdPropertyForInstance, IGuidGetter guidGetter, IFileSystem fileSystem)
+            : base(xmlFileSerializationHelper, getDataPathForType, getValueOfIdPropertyForInstance, guidGetter, fileSystem)
+        {
+        }
+
+        public IQueryable<ToolLinkProviderDraft> GetAll()
+        {
+            var toolLinks = cache["ToolLinks"] as IQueryable<ToolLinkProviderDraft>;
+
+            if (toolLinks == null)
+            {
+                toolLinks = base.GetAll().AsQueryable();
+
+                var localWorkingFolder = Path.Combine(ConfigurationManager.AppSettings["Bennington.LocalWorkingFolder"], @"BenningtonData\");
+                var policy = new CacheItemPolicy();
+
+                policy.ChangeMonitors.Add(new HostFileChangeMonitor(new List<string> { localWorkingFolder }));
+
+                cache.Add("ToolLinks", toolLinks, policy);
+            }
+
+            return toolLinks;
+        }
+    }
 }
