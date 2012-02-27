c5856abdf9e03d90304e2c07c4d75e4cf4af6deb
Ferris Lucas <ferris.lucas@gmail.com>
2012-02-27 14:20:29 -0600

    add reports management icon to cms top right menu

---
+++ src/Bennington.Cms.ReportManagement/IconMenuRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Bennington.Cms.MenuSystem;
+
+namespace Bennington.Cms.ReportManagement
+{
+    public class MenuSystemConfigurer : IMenuSystemConfigurer
+    {
+        public void Configure(IMenuRegistry sectionMenuRegistry)
+        {
+            sectionMenuRegistry.Add(new ActionIconMenuItem("Report Management", "~/Content/Canvas/icondemo.png", "Index", "ReportManagement"));
+        }
+    }
+}
